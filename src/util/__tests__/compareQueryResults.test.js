eb1e394ce8b091edd5baf10eafe2222b1ab2ec14
Laura Keil <Laura.Keil@trivago.com>
2020-07-23 13:08:20 +0200

    use Inline Snapshots

--- src/util/__tests__/compareQueryResults.test.js
+++ src/util/__tests__/compareQueryResults.test.js
@@ -11,114 +11,81 @@ const collectibleValidWithResultsDifferentResults = require("../__mockData__/ser
  * Unit tests for compareQueryResults
  */
 
-it("query results are identical", () => {
-  expect.assertions(1);
-  return compareQueryResults(
-    collectibleValidWithResultsSameResults,
-    collectibleValidWithResultsSameResults.queryResults1,
-    collectibleValidWithResultsSameResults.queryResults2
-  ).then((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        queryResult1: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        queryResult2: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        test_results: true,
-      })
-    );
+describe("Unit tests for compareQueryResults", () => {
+  it("query results are identical", () => {
+    expect.assertions(1);
+    return compareQueryResults(
+      collectibleValidWithResultsSameResults,
+      collectibleValidWithResultsSameResults.queryResults1,
+      collectibleValidWithResultsSameResults.queryResults2
+    ).then((collectible) => {
+      expect(collectible).toMatchInlineSnapshot(`
+        Object {
+          "query1": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+          "query2": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+          "queryResult1": Array [
+            Object {
+              "address": "Nordweg 5",
+              "city": "Bremen",
+              "firstName": "Renate",
+              "id": 1,
+              "lastName": "Weg",
+            },
+          ],
+          "queryResult2": Array [
+            Object {
+              "address": "Nordweg 5",
+              "city": "Bremen",
+              "firstName": "Renate",
+              "id": 1,
+              "lastName": "Weg",
+            },
+          ],
+          "test_results": true,
+        }
+      `);
+    });
   });
-});
 
-it("query results are identical", () => {
-  expect.assertions(1);
-  return compareQueryResults(
-    collectibleValidWithResultsSameResults,
-    collectibleValidWithResultsSameResults.queryResults1,
-    collectibleValidWithResultsSameResults.queryResults2
-  ).then((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        queryResult1: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        queryResult2: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        test_results: true,
-      })
-    );
-  });
-});
-
-it("query results are not identical", () => {
-  expect.assertions(1);
-  return compareQueryResults(
-    collectibleValidWithResultsDifferentResults,
-    collectibleValidWithResultsDifferentResults.queryResult1,
-    collectibleValidWithResultsDifferentResults.queryResult2
-  ).then((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "SELECT * FROM person ORDER BY lastName DESC LIMIT 2",
-        queryResult1: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        queryResult2: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-          {
-            id: 2,
-            lastName: "Weg",
-            firstName: "Heiko",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        test_results: false,
-      })
-    );
+  it("query results are not identical", () => {
+    expect.assertions(1);
+    return compareQueryResults(
+      collectibleValidWithResultsDifferentResults,
+      collectibleValidWithResultsDifferentResults.queryResult1,
+      collectibleValidWithResultsDifferentResults.queryResult2
+    ).then((collectible) => {
+      expect(collectible).toMatchInlineSnapshot(`
+        Object {
+          "query1": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+          "query2": "SELECT * FROM person ORDER BY lastName DESC LIMIT 2",
+          "queryResult1": Array [
+            Object {
+              "address": "Nordweg 5",
+              "city": "Bremen",
+              "firstName": "Renate",
+              "id": 1,
+              "lastName": "Weg",
+            },
+          ],
+          "queryResult2": Array [
+            Object {
+              "address": "Nordweg 5",
+              "city": "Bremen",
+              "firstName": "Renate",
+              "id": 1,
+              "lastName": "Weg",
+            },
+            Object {
+              "address": "Nordweg 5",
+              "city": "Bremen",
+              "firstName": "Heiko",
+              "id": 2,
+              "lastName": "Weg",
+            },
+          ],
+          "test_results": false,
+        }
+      `);
+    });
   });
 });
--- src/util/__tests__/handlePayload.test.js
+++ src/util/__tests__/handlePayload.test.js
@@ -13,85 +13,91 @@ const collectibleValidWithoutResultsDifferentResults = require("../__mockData__/
  * Also covers: connectToDatabase, getQueryResults and disconnectFromDatabase
  */
 
-it("Request is handled correctly (without error) and query results are identical", () => {
-  expect.assertions(1);
-  return handlePayload(collectibleValidWithoutResultsSameResults).then((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        queryResult1: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        queryResult2: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        test_results: true,
-      })
-    );
+describe("Unit tests for handlePayload", () => {
+  it("Request is handled correctly (without error) and query results are identical", () => {
+    expect.assertions(1);
+    return handlePayload(collectibleValidWithoutResultsSameResults).then((collectible) => {
+      expect(collectible).toMatchInlineSnapshot(`
+          Object {
+            "error": undefined,
+            "query1": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+            "query2": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+            "queryResult1": Array [
+              Object {
+                "address": "Nordweg 5",
+                "city": "Bremen",
+                "firstName": "Renate",
+                "id": 1,
+                "lastName": "Weg",
+              },
+            ],
+            "queryResult2": Array [
+              Object {
+                "address": "Nordweg 5",
+                "city": "Bremen",
+                "firstName": "Renate",
+                "id": 1,
+                "lastName": "Weg",
+              },
+            ],
+            "test_results": true,
+          }
+        `);
+    });
   });
-});
 
-it("Request is handled correctly (without error) and query results are not identical", () => {
-  expect.assertions(1);
-  return handlePayload(collectibleValidWithoutResultsDifferentResults).then((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "SELECT * FROM person ORDER BY lastName DESC LIMIT 2;",
-        queryResult1: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        queryResult2: [
-          {
-            id: 1,
-            lastName: "Weg",
-            firstName: "Renate",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-          {
-            id: 2,
-            lastName: "Weg",
-            firstName: "Heiko",
-            address: "Nordweg 5",
-            city: "Bremen",
-          },
-        ],
-        test_results: false,
-      })
-    );
+  it("Request is handled correctly (without error) and query results are not identical", () => {
+    expect.assertions(1);
+    return handlePayload(collectibleValidWithoutResultsDifferentResults).then((collectible) => {
+      expect(collectible).toMatchInlineSnapshot(`
+          Object {
+            "error": undefined,
+            "query1": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+            "query2": "SELECT * FROM person ORDER BY lastName DESC LIMIT 2;",
+            "queryResult1": Array [
+              Object {
+                "address": "Nordweg 5",
+                "city": "Bremen",
+                "firstName": "Renate",
+                "id": 1,
+                "lastName": "Weg",
+              },
+            ],
+            "queryResult2": Array [
+              Object {
+                "address": "Nordweg 5",
+                "city": "Bremen",
+                "firstName": "Renate",
+                "id": 1,
+                "lastName": "Weg",
+              },
+              Object {
+                "address": "Nordweg 5",
+                "city": "Bremen",
+                "firstName": "Heiko",
+                "id": 2,
+                "lastName": "Weg",
+              },
+            ],
+            "test_results": false,
+          }
+        `);
+    });
   });
-});
 
-it("Request is handled correctly (with error)", () => {
-  expect.assertions(1);
-  return handlePayload(collectibleInvalidWithoutResults_1).catch((collectible) => {
-    expect(JSON.stringify(collectible)).toEqual(
-      JSON.stringify({
-        query1: "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
-        query2: "* FROM person ORDER BY lastName DESC LIMIT 1",
-        test_results: false,
-        error: "query2 is not a SELECT-statement",
-      })
-    );
+  it("Request is handled correctly (with error)", () => {
+    expect.assertions(1);
+    return handlePayload(collectibleInvalidWithoutResults_1).catch((collectible) => {
+      expect(collectible).toMatchInlineSnapshot(`
+          Object {
+            "error": "query2 is not a SELECT-statement",
+            "query1": "SELECT * FROM person ORDER BY lastName DESC LIMIT 1",
+            "query2": "* FROM person ORDER BY lastName DESC LIMIT 1",
+            "queryResult1": undefined,
+            "queryResult2": undefined,
+            "test_results": false,
+          }
+        `);
+    });
   });
 });
